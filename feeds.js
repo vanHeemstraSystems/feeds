2008ef97e926bec76657282860a83f82e51fe103
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-05-02 17:23:13 +0100

    Modified

--- feeds.js
+++ feeds.js
@@ -18,24 +18,3 @@ Feeds.prototype.feed = function() {
 }
 
 module.exports = Feeds;
-
-
-// REMOVE ALL BELOW
-
-// /*
-//  * Feeds
-//  * 
-//  * param: database (e.g. 'rethinkdb')
-//  */
-// module.exports = function(database) {
-//   var database = toLowerCase(database);
-//   var _Feeds = {};
-//   var path = require('../libraries/path');
-//   var paths = require('../paths/paths');
-//   config = require(path.join(paths.configurations, '/configurations.js'))(database);
-//   var common = config.common,
-//   server_prefix = common.server_prefix || 'PREFIX';
-//   console.log(server_prefix + " - Feeds database required.");
-//   _Feeds.database = require('./' + database + '.js');
-//   return _Feeds;
-// };//does not call itself
